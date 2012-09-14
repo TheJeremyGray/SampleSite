0d5942c02d165d07e544d241815473e6cb6b5200
Jeremy Gray <jeremy.gray@gmail.com>
2012-09-14 13:15:53 -0400

    Initial

---
+++ MySampleSite/MySampleSite/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Elmah;
+
+namespace MySampleSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "";
+           
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Administrators")]
+        public ActionResult Administration()
+        {
+            return View();
+        }
+    }
+}
